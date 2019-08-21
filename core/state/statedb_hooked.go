0a3c14eaeec7901e146ed9ab6e518d952710d733
mark.lin <mark@maicoin.com>
2019-08-21 09:28:12 +0800

    core/state: keep transfer log in state db

--- core/state/statedb_hooked.go
+++ core/state/statedb_hooked.go
@@ -308,3 +308,11 @@ func (s *hookedStateDB) IntermediateRoot(deleteEmptyObjects bool) common.Hash {
 func (s *hookedStateDB) IsAddressInMutations(addr common.Address) bool {
 	return s.inner.IsAddressInMutations(addr)
 }
+
+func (s *hookedStateDB) AddTransferLog(log *types.TransferLog) {
+	// The inner will modify the log (add fields), so invoke that first
+	s.inner.AddTransferLog(log)
+	if s.hooks.OnTransferLog != nil {
+		s.hooks.OnTransferLog(log)
+	}
+}
