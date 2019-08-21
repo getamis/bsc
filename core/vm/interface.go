2babfb75b8fd37af302e3fc674e184346786aa98
mark.lin <mark@maicoin.com>
2019-08-21 09:28:12 +0800

    core/state: keep transfer log in state db
    (cherry picked from commit 141cb51d31f82b5a525fc089e518faa4e86cd3c0)

--- core/vm/interface.go
+++ core/vm/interface.go
@@ -80,6 +80,8 @@ type StateDB interface {
 
 	AddLog(*types.Log)
 	AddPreimage(common.Hash, []byte)
+
+	AddTransferLog(*types.TransferLog)
 }
 
 // CallContext provides a basic interface for the EVM calling conventions. The EVM
