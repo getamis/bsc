ab451cd876ac66fe8817e1f7ed4d81f692f3d9d2
ian <ian@maicoin.com>
2021-06-03 20:07:54 +0800

    params: fix CheckConfigForkOrder for pass through AllEthashProtocolChanges

--- params/config.go
+++ params/config.go
@@ -634,14 +634,24 @@ func (c *ChainConfig) CheckConfigForkOrder() error {
 		optional bool // if true, the fork may be nil and next fork is still allowed
 	}
 	var lastFork fork
+	bscFork := false
 	for _, cur := range []fork{
 		{name: "mirrorSyncBlock", block: c.MirrorSyncBlock},
 		{name: "brunoBlock", block: c.BrunoBlock},
 		{name: "berlinBlock", block: c.BerlinBlock},
 	} {
 		if lastFork.name != "" {
+			if lastFork.name == "ramanujanBlock" || lastFork.name == "mirrorSyncBlock" {
+				if lastFork.block != nil {
+					bscFork = true
+				}
+			}
 			// Next one must be higher number
 			if lastFork.block == nil && cur.block != nil {
+				// To pass through AllEthashProtocolChanges
+				if !bscFork && cur.name == "berlinBlock" {
+					return nil
+				}
 				return fmt.Errorf("unsupported fork ordering: %v not enabled, but %v enabled at %v",
 					lastFork.name, cur.name, cur.block)
 			}
