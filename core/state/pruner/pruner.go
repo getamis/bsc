7aad0ddae185a29433629de6f912cdb1c3af07ea
Cara Wang <cara@maicoin.com>
2022-04-13 10:33:27 +0800

    fix: backUpOldDb with transfer logs

--- core/state/pruner/pruner.go
+++ core/state/pruner/pruner.go
@@ -335,8 +335,12 @@ func (p *BlockPruner) backUpOldDb(name string, cache, handles int, namespace str
 		if td == nil {
 			return consensus.ErrUnknownAncestor
 		}
+		l, err := rawdb.ReadTransferLogs(chainDb, blockHash, blockNumber)
+		if err != nil {
+			return err
+		}
 		// Write into new ancient_back db.
-		rawdb.WriteAncientBlock(frdbBack, block, receipts, td)
+		rawdb.WriteAncientBlock(frdbBack, block, receipts, td, l)
 		// Print the log every 5s for better trace.
 		if common.PrettyDuration(time.Since(start)) > common.PrettyDuration(5*time.Second) {
 			log.Info("block backup process running successfully", "current blockNumber for backup", blockNumber)
