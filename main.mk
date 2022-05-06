gaga5lala/go-ethereum

Makefile: make docker and docker.push

main.mk
@@ -1,0 +1,5 @@
+DOCKER_REPOSITORY := quay.io/amis
+DOCKER_IMAGE := $(DOCKER_REPOSITORY)/indexer-geth
+ifeq ($(DOCKER_IMAGE_TAG),)
+DOCKER_IMAGE_TAG := $(shell git rev-parse --short HEAD 2> /dev/null)
+endif

getamis/bsc

main.mk
@@ -1,0 +1,5 @@
+DOCKER_REPOSITORY := quay.io/amis
+DOCKER_IMAGE := $(DOCKER_REPOSITORY)/indexer-bsc
+ifeq ($(DOCKER_IMAGE_TAG),)
+DOCKER_IMAGE_TAG := $(shell git rev-parse --short HEAD 2> /dev/null)
+endif