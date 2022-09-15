12567a150125c934054f98368d1512ebbcc96a49
Cara Wang <cara@maicoin.com>
2022-09-15 09:51:19 +0800

    core, core/rawdb, ethdb: add transfer logs in freezer
    (cherry picked from commit a9dd564ed84d1bb835d17386aaa00e05bc809dcd)
    
    # Conflicts:
    #	core/blockchain.go
    #	core/rawdb/accessors_chain.go
    #	core/rawdb/database.go
    #	core/rawdb/freezer.go
    #	core/rawdb/schema.go
    #	eth/api.go

--- core/blockchain_reader.go
+++ core/blockchain_reader.go
@@ -19,6 +19,7 @@ package core
 import (
 	"math/big"
 
+	ethereum "github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/core/rawdb"
@@ -447,4 +448,17 @@ func (bc *BlockChain) SubscribeBlockProcessingEvent(ch chan<- bool) event.Subscr
 // SubscribeFinalizedHeaderEvent registers a subscription of FinalizedHeaderEvent.
 func (bc *BlockChain) SubscribeFinalizedHeaderEvent(ch chan<- FinalizedHeaderEvent) event.Subscription {
 	return bc.scope.Track(bc.finalizedHeaderFeed.Subscribe(ch))
+}
+
+func (bc *BlockChain) GetTransferLogs(hash common.Hash) ([]*types.TransferLog, error) {
+	number := rawdb.ReadHeaderNumber(bc.db, hash)
+	if number == nil {
+		return nil, ethereum.NotFound
+	}
+	transferLogs, err := rawdb.ReadTransferLogs(bc.db, hash, *number)
+	if err != nil {
+		return nil, err
+	}
+	bc.transferLogsCache.Add(hash, transferLogs)
+	return transferLogs, nil
 }
