2de72e25dc0135fef23af96233969bf9f964b625
Cara Wang <cara@maicoin.com>
2022-09-15 09:51:19 +0800

    core, core/rawdb, ethdb: add transfer logs in freezer

--- core/blockchain_reader.go
+++ core/blockchain_reader.go
@@ -22,6 +22,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/log"
 
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/core/rawdb"
@@ -537,4 +538,17 @@ func (bc *BlockChain) AncientTail() (uint64, error) {
 		return 0, err
 	}
 	return tail, nil
+}
+
+func (bc *BlockChain) GetTransferLogs(hash common.Hash) ([]*types.TransferLog, error) {
+	number := rawdb.ReadHeaderNumber(bc.db, hash)
+	if number == nil {
+		return nil, ethereum.NotFound
+	}
+	transferLogs, err := rawdb.ReadTransferLogs(bc.db, hash, *number)
+	if err != nil {
+		return nil, err
+	}
+	bc.transferLogsCache.Add(hash, transferLogs)
+	return transferLogs, nil
 }
