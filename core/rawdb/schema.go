e016dcabad64ac1752eff7cbc456f6572856f94b
Cara Wang <cara@maicoin.com>
2022-09-15 09:51:19 +0800

    core, core/rawdb, ethdb: add transfer logs in freezer

--- core/rawdb/schema.go
+++ core/rawdb/schema.go
@@ -137,16 +137,20 @@ const (
 
 	// freezerDifficultyTable indicates the name of the freezer total difficulty table.
 	freezerDifficultyTable = "diffs"
+
+	// freezerTransferLogTable indicates the name of the freezer transfer logs table.
+	freezerTransferLogTable = "transfers"
 )
 
 // FreezerNoSnappy configures whether compression is disabled for the ancient-tables.
 // Hashes and difficulties don't compress well.
 var FreezerNoSnappy = map[string]bool{
-	freezerHeaderTable:     false,
-	freezerHashTable:       true,
-	freezerBodiesTable:     false,
-	freezerReceiptTable:    false,
-	freezerDifficultyTable: true,
+	freezerHeaderTable:      false,
+	freezerHashTable:        true,
+	freezerBodiesTable:      false,
+	freezerReceiptTable:     false,
+	freezerDifficultyTable:  true,
+	freezerTransferLogTable: false,
 }
 
 // LegacyTxLookupEntry is the legacy TxLookupEntry definition with some unnecessary
@@ -202,6 +206,11 @@ func blockReceiptsKey(number uint64, hash common.Hash) []byte {
 // diffLayerKey = diffLayerKeyPrefix + hash
 func diffLayerKey(hash common.Hash) []byte {
 	return append(diffLayerPrefix, hash.Bytes()...)
+}
+
+// blockTransferLogsKey = blockReceiptsPrefix + num (uint64 big endian) + hash
+func blockTransferLogsKey(number uint64, hash common.Hash) []byte {
+	return append(append(blockTranferLogsPrefix, encodeBlockNumber(number)...), hash.Bytes()...)
 }
 
 // txLookupKey = txLookupPrefix + hash
