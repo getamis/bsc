4a182b066e0be745badfa8bf61b380a6dac8c000
mark.lin <mark@maicoin.com>
2023-04-27 09:25:57 +0800

    Revert "*: add broadcast local txs feature"
    This reverts commit 9e99913220252c0239e5c71903be43bacfe7cba4.

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -92,7 +92,6 @@ var (
 		utils.TxPoolNoLocalsFlag,
 		utils.TxPoolJournalFlag,
 		utils.TxPoolRejournalFlag,
-		utils.TxPoolBroadcastPendingLocalTxFlag,
 		utils.TxPoolPriceLimitFlag,
 		utils.TxPoolPriceBumpFlag,
 		utils.TxPoolAccountSlotsFlag,
